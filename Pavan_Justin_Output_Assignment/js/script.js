02b9e0fb25396fa60062d01f67768bdef8e2cbf0
jlpavan <Justin@Justins-MacBook-Pro.local>
2015-10-01 23:17:11 +0300

    If statement for correct response.
    I created an if statement that compared a variable to my boolean
    variable to determine which response to output.

--- Pavan_Justin_Output_Assignment/js/script.js
+++ Pavan_Justin_Output_Assignment/js/script.js
@@ -6,12 +6,23 @@
 //creating a variable that represents a boolean value of true
     var booleanValue = true;
 
+//Creating a string variable called school
+    var schoolName = "Full Sail University";
+
 //creating a string variable for how many years I have been in the army
-    var armyYears = 5;
+    var armyYears = 4;
 
 //prompt for string variable for user's name
     var usersName = prompt("Hello, my name is Justin Pavan. What is your name?");
 
 //prompt for a a string variable to compare to booleanValue
-    var userResponse = prompt("Hello ",+ userName + "Are you my instructor?");
+    var userResponse = prompt("Hello " + usersName + ", Are you my instructor?");
+
+//comparing the conditions of booleanValue to usersName to determine response
+    if(booleanValue == usersResponse) {
+        alert("Hi Professor, let me tell you about myself. I am currently in the Army. " +
+            "\nI have been in the Army for ," + armyYears
+        ". I enrolled at " + schoolName +
+        " \nbecause I wanted to learn Mobile Development. I am very Excited to be in your class!"
+    }
 
