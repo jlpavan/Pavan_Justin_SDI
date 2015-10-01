fc6b1607158f6a9ad7982bc98ed42841a53bcd72
jlpavan <Justin@Justins-MacBook-Pro.local>
2015-10-01 22:46:35 +0300

    Script error adjustment
    Adjusted the script code within the index.html to correctly pull script
    from the js directory.

--- Pavan_Justin_Output_Assignment/js/script.js
+++ Pavan_Justin_Output_Assignment/js/script.js
@@ -1,3 +1,12 @@
 /**
- * Created by Justin on 10/1/15.
+ * Justin Pavan
+ * SDI-1510
+ * Output Assignment
  */
+//creating a variable that represents a boolean value of true
+    var booleanValue = true;
+
+//prompt for string variable for user's name
+    var usersName = prompt("Hello, my name is Justin Pavan. What is your name?");
+
+//prompt for string variable to compare to booleanValue
