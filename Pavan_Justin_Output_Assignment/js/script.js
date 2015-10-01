5c5efada6db1e7022ef0e23115b774a33ecb6d54
jlpavan <Justin@Justins-MacBook-Pro.local>
2015-10-01 22:52:53 +0300

    number variable
    Added a number variable that states how many years i have been in the
    army.

--- Pavan_Justin_Output_Assignment/js/script.js
+++ Pavan_Justin_Output_Assignment/js/script.js
@@ -6,7 +6,11 @@
 //creating a variable that represents a boolean value of true
     var booleanValue = true;
 
+//creating a string variable for how many years I have been in the army
+    var armyYears = 5;
+
 //prompt for string variable for user's name
     var usersName = prompt("Hello, my name is Justin Pavan. What is your name?");
 
 //prompt for string variable to compare to booleanValue
+    
