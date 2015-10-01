56db011324d8e545d671ebbda6b83c4e56ed32b1
jlpavan <Justin@Justins-MacBook-Pro.local>
2015-10-02 00:09:35 +0300

    Corrects
    Added a few changes to the responses to make them appear appropriately

--- Pavan_Justin_Output_Assignment/js/script.js
+++ Pavan_Justin_Output_Assignment/js/script.js
@@ -15,14 +15,19 @@
 //prompt for string variable for user's name
     var usersName = prompt("Hello, my name is Justin Pavan. What is your name?");
 
+
 //prompt for a a string variable to compare to booleanValue
-    var usersResponse = prompt("Hello " + usersName + ", Are you my instructor? Answer true or false");
+    var usersResponse = prompt("Hello " + usersName + ", Are you my instructor? Answer Yes or No");
+
+//setting usersResponse to be lower case.
+var usersResponse = usersResponse.toLowerCase();
 
 //comparing the conditions of booleanValue to usersName to determine response
-    if(booleanValue == usersResponse){
-        alert("Hi Professor, let me tell you about myself. I am currently in the Army.\nI have been in the Army for ," + armyYears + "years.I enrolled at " + schoolName +
-            " \nbecause I wanted to learn Mobile Development. I am very Excited to be in your class!")
+    if(usersResponse == "yes"){
+        alert("Hi Professor, let me tell you about myself. I am currently in the Army.I have been in the Army for " + armyYears + "years. I enrolled at " + schoolName +
+            " because I wanted to learn Mobile Development. I am very excited to be in your class!")
     }else{
         alert("Sorry, I must have the wrong person, Goodbye!")
     }
+
 
