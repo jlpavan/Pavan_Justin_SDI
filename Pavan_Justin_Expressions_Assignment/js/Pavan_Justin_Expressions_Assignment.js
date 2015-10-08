bd1e220fb8299f08af3f46897f73b9d101a6deb7
jlpavan <Justin@Justins-MacBook-Pro.local>
2015-10-08 23:45:41 +0300

    Change in order
    I moved the conditions for the variable sex so that if the user inputs
    an invalid response it will notify him or her after clicking okay
    instead of waiting till after they put in all their information.

--- Pavan_Justin_Expressions_Assignment/js/Pavan_Justin_Expressions_Assignment.js
+++ Pavan_Justin_Expressions_Assignment/js/Pavan_Justin_Expressions_Assignment.js
@@ -10,6 +10,22 @@
 //variables used to complete the program
 var myCalculations = [.45,.025,1.20,.23,10.8,5.4]; //the various numbers needed for BMI and body fat calculations
 var sex = prompt("Are you Male or female?");//necessary for determining calculation for BF%
+
+//converting sex to a number value
+
+sex = sex.toLowerCase(); //Converting sex to all lower case for more accurate calculations
+
+if(sex !== "male" && sex !== "female"){
+    alert("You did not select a valid gender. Please refresh the page and try again.")}
+if(sex == "male"){
+    sex = 1;//setting sex to 1 in the case of male for calculations
+}
+if(sex == "female") {
+    sex = 0;// setting sex to 0 in the case of female for calculations
+}
+
+//additional variables
+
 var age = prompt("How old are you?");//necessary for determining calculations for BF%
 var height = prompt("How tall are you in inches?");//necessary for determining BMI
 var weight = prompt("How much do you weight in pounds?");//necessary for determining BMI
@@ -25,25 +41,12 @@ weight *= myCalculations[0];//converting weight to kilograms
 height *= myCalculations[1];//converting height to meters
 height *= height;//multiplying height to itself
 bmi = weight / height; //dividing weight by height to get bmi
-bmi = bmi.toFixed();
-
-//converting sex to a number value
-
-sex = sex.toLowerCase(); //Converting sex to all lower case for more accurate calculations
-
-if(sex !== "male" && sex !== "female"){
-    alert("You did not select a valid gender. Please refresh the page and try again.")}
-if(sex == "male"){
-    sex = 1;//setting sex to 1 in the case of male for calculations
-}
-if(sex == "female") {
-    sex = 0;// setting sex to 0 in the case of female for calculations
-}
+bmi = bmi.toFixed();//Converted bmi to round to the nearest integer
 
 //Calculations for Body fat percentage
-
 var bodyFat =  myCalculations[2] * bmi + myCalculations[3] * age - myCalculations[4] * sex - myCalculations[5];
-bodyFat = bodyFat.toFixed();
+
+bodyFat = bodyFat.toFixed();//converted bodyFat to round to the nearest integerl
 
 //alert to the user his or her results
 var results = ("Your body fat percentage is " + bodyFat + "% and have a BMI of " + bmi+".");
