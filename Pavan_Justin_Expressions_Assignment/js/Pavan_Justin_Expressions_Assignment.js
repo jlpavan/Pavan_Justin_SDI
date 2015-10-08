91c0739028038003a36c1a84e2257adcf360dbc5
jlpavan <Justin@Justins-MacBook-Pro.local>
2015-10-08 22:17:11 +0300

    BMI Calculations
    Here I took the formula to calculate BMI and applied it to users input.

--- Pavan_Justin_Expressions_Assignment/js/Pavan_Justin_Expressions_Assignment.js
+++ Pavan_Justin_Expressions_Assignment/js/Pavan_Justin_Expressions_Assignment.js
@@ -6,12 +6,19 @@
  * This program is used to calculate an adults body fat % using bmi, age, and gender.
  */
 
-var myCalculations = [.45,.025,1.20,.23,10.8]; //the various numbers needed for BMI and body fat calculations
-var sex = prompt("Are you Male or female?");
-var age = prompt("How old are you?");
-var height = prompt("How tall are you in inches?");
-var weight = prompt("How much do you weight in pounds?");
 
+//variables used to complete the program
+var myCalculations = [.45,.025,1.20,.23,10.8]; //the various numbers needed for BMI and body fat calculations
+var sex = prompt("Are you Male or female?");//necessary for determining calculation for BF%
+var age = prompt("How old are you?");//necessary for determining calculations for BF%
+var height = prompt("How tall are you in inches?");//necessary for determining BMI
+var weight = prompt("How much do you weight in pounds?");//necessary for determining BMI
+
+//calculations for BMI
+weight *= myCalculations[0];//converting weight to kilograms
+height *= myCalculations[1];//converting height to meters
+height *= height;//multiplying height to itself
+bmi = weight / height; //dividing weight by height to get bmi
 
 
 
