ab9a01bb93ff60cd8b9f4e3033a264e3be5b0f8a
jlpavan <Justin@Justins-MacBook-Pro.local>
2015-10-08 22:01:44 +0300

    Array and Variables
    Created an array that holds the numbers necessary for calculations and
    variables inputed by the user for calculations

--- Pavan_Justin_Expressions_Assignment/js/Pavan_Justin_Expressions_Assignment.js
+++ Pavan_Justin_Expressions_Assignment/js/Pavan_Justin_Expressions_Assignment.js
@@ -1,3 +1,18 @@
 /**
- * Created by Justin on 10/8/15.
+ * Justin Pavan
+ * 10/08/2015
+ * Expressions Assignment
+ *
+ * This program is used to calculate an adults body fat % using bmi, age, and gender.
  */
+
+var myCalculations = [.45,.025,1.20,.23,10.8]; //the various numbers needed for BMI and body fat calculations
+var sex = prompt("Are you Male or female?");
+var age = prompt("How old are you?");
+var height = prompt("How tall are you in inches?");
+var weight = prompt("How much do you weight in pounds?");
+
+
+
+
+
