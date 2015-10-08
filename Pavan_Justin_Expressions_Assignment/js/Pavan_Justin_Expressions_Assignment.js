40650161d275cbde1603e41a369abd24ef510ee7
jlpavan <Justin@Justins-MacBook-Pro.local>
2015-10-08 20:26:34 +0300

    arrays for allowed body fat percentage
    I created arrays that represent the maximum allowed body fat
    percentages for male and female.

--- Pavan_Justin_Expressions_Assignment/js/Pavan_Justin_Expressions_Assignment.js
+++ Pavan_Justin_Expressions_Assignment/js/Pavan_Justin_Expressions_Assignment.js
@@ -4,3 +4,5 @@
  * Expressions Assignment
  */
 
+var maleAllowedBFP = [20,22,24,26];   //allowable male body fat percentage for ages [17-20,21-27,28-39,40<]
+var femaleAllowedBFP = [30,32,34,36]; //allowable female body fat percentage for ages [17-20,21-27,28-39,40<]
