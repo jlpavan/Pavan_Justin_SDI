493209b358e7decaad8ccaa69f4ff776efc398c1
jlpavan <Justin@Justins-MacBook-Pro.local>
2015-10-16 00:14:05 +0300

    Variables
    Adding variables that allow the user to input values when prompted.

--- Pavan_Justiin_Conditionals_Assignment/js/Pavan_Justin_Conditionals_Assignment.js
+++ Pavan_Justiin_Conditionals_Assignment/js/Pavan_Justin_Conditionals_Assignment.js
@@ -1,3 +1,17 @@
 /**
- * Created by Justin on 10/15/15.
+ * Justin
+ * 10/15/15.
+ * Conditionals Assignment
+ *
+ * This Program is made to determine how many boxes of a specific size can fit in a container of that is 7 feet wide,
+ * 10 feet in length, and 8 feet tall.
  */
+
+alert("To determine how many of a particular box you can fit into a container that is 7 feet wide, 10 feet in lenght, and 8 feet tall, " +
+    "please insert the information requested when required.");
+// Variables
+var containLenght = (967680); //The volume of the container in inches
+var boxLength = prompt("What is the length of your box in inches?");//User's box length
+var boxWidth = prompt("what is the width of your box in inches?");  //User's box width
+var boxHeight = prompt("What is the height of your box in inches?"); //User's box height
+
