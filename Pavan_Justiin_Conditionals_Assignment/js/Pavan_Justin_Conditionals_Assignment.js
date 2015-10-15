774859400a7ac8b719296f4cd9f2a7159e59c5cf
jlpavan <Justin@Justins-MacBook-Pro.local>
2015-10-16 00:41:19 +0300

    Validating variables
    converted variables into number variables and created a while loop to
    test to see if there are any non number characters in the values of the
    variables.

--- Pavan_Justiin_Conditionals_Assignment/js/Pavan_Justin_Conditionals_Assignment.js
+++ Pavan_Justiin_Conditionals_Assignment/js/Pavan_Justin_Conditionals_Assignment.js
@@ -10,8 +10,33 @@
 alert("To determine how many of a particular box you can fit into a container that is 7 feet wide, 10 feet in lenght, and 8 feet tall, " +
     "please insert the information requested when required.");
 // Variables
-var containLenght = (967680); //The volume of the container in inches
-var boxLength = prompt("What is the length of your box in inches?");//User's box length
-var boxWidth = prompt("what is the width of your box in inches?");  //User's box width
-var boxHeight = prompt("What is the height of your box in inches?"); //User's box height
+var containVolume = (967680); //The volume of the container in inches
+var boxLength = prompt("What is the length of your box in inches?");//User's insert for box length.
+
+boxLength = Number(boxLength);//converting boxLegth from string to number type.
+
+//Validating that the the variable boxLength is a number variable with no string variables
+while(isNaN(boxLength)){
+    boxLength = prompt("You did not enter a valid number. Please try again?"); //asking the user to insert another value
+}
+var boxWidth = prompt("what is the width of your box in inches?");  // user's input for box's width
+
+boxWidth = Number(boxWidth);//converting boxWidth from a string to a number variabl.
+
+//Validating that the variable boxWidth is a number variable with no string variables
+while(isNaN(boxWidth)){
+    boxWidth = prompt("You did not enter a valid number. Please try again?");//asking the user to insert another value
+}
+var boxHeight = prompt("What is the height of your box in inches?"); //User's input for box's height
+
+boxHeight = Number(boxHeight);//converting boxHeight from a string to a number variabl.
+
+//Validating that the variable boxLength is a number variable with no string variables
+while(isNaN(boxWidth)){
+    boxWidth = prompt("You did not enter a valid number. Please try again?");//asking the user to insert another value
+}
+
+
+
+
 
