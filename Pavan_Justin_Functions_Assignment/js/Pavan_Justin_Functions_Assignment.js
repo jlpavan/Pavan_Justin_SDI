4758136416b3a977e2fffa2b3b409b678f59602d
jlpavan <Justin@Justins-MacBook-Pro.local>
2015-10-24 20:41:12 +0300

    Setting up Functions assignment
    Setting up the directory for functions assignment. While also creating
    a variable for user input and using a while loop to validate the input.

---
+++ Pavan_Justin_Functions_Assignment/js/Pavan_Justin_Functions_Assignment.js
@@ -0,0 +1,13 @@
+/**
+ * Justin
+ * 10/24/15.
+ * Functions Assignment
+ */
+
+//Variables
+var player = prompt("Hello, What is your name?");
+
+while(player === ""){
+    player = prompt("You did not enter your name.\nPlease enter name below.");
+}
+
