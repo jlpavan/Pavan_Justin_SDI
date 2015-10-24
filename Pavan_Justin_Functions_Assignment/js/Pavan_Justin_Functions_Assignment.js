660a983ff60683b4903f0977b083a6e89b594b6e
jlpavan <Justin@Justins-MacBook-Pro.local>
2015-10-24 21:10:28 +0300

    created var gameDecision
    set up gameDecision to determine if user wants to play, created a while
    look for a valid response. then a conditional statement to determine
    which game to play using the variable gameName

--- Pavan_Justin_Functions_Assignment/js/Pavan_Justin_Functions_Assignment.js
+++ Pavan_Justin_Functions_Assignment/js/Pavan_Justin_Functions_Assignment.js
@@ -8,6 +8,21 @@
 var player = prompt("Hello, What is your name?");
 
 while(player === ""){
-    player = prompt("You did not enter your name.\nPlease enter name below.");
+    player = prompt("You did not enter your name.\n\nPlease enter name below.");
 }
 
+var gameDecision = prompt("Hi " + player + " Would you like to play the Powerball or the Florida Lottery?\n\nPlease answer Yes or No.");
+
+gameDecision = gameDecision.toLowerCase();
+
+while(gameDecision !== "yes" && gameDecision !== "no"){
+    gameDecision = prompt("You did not enter a valid response.\n\nPlease enter Yes to play, or No to decline.");
+}
+
+if(gameDecision == "no"){
+    alert("Okay, Thank you for visiting. Come again soon!");
+}else{
+    var gameName = prompt("That is great!\nTo play the Powerball, please type:\t Powerball.\nTo play Florida Lottery, please type:\t Florida");
+}
+
+gameName = gameName.toLowerCase();
