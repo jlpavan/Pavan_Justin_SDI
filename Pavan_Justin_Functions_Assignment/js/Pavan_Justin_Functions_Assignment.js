f08a3d3ac3022085470f5be9972af27b8f3a76b3
jlpavan <Justin@Justins-MacBook-Pro.local>
2015-10-24 21:51:09 +0300

    Correctiosn
    corrected the function look to account for 6 numbers instead of 5, and
    added a statement to the variables while loops to help better correct
    input.

--- Pavan_Justin_Functions_Assignment/js/Pavan_Justin_Functions_Assignment.js
+++ Pavan_Justin_Functions_Assignment/js/Pavan_Justin_Functions_Assignment.js
@@ -8,7 +8,7 @@
 function gameNumbers(min, max) {
     var randomNumber = [];
 
-    for (var i = 0; i < 5; i++){
+    for (var i = 0; i < 6 ; i++){
         randomNumber[i] = Math.random()* (max - min) + min;
         randomNumber[i] = Math.round(randomNumber[i]);
     }
@@ -27,6 +27,7 @@ gameDecision = gameDecision.toLowerCase();
 
 while(gameDecision !== "yes" && gameDecision !== "no"){
     gameDecision = prompt("You did not enter a valid response.\n\nPlease enter Yes to play, or No to decline.");
+    gameDecision = gameDecision.toLowerCase()
 }
 
 if(gameDecision == "no"){
@@ -37,7 +38,14 @@ if(gameDecision == "no"){
 
 gameName = gameName.toLowerCase();
 
-while(gameName !== "powerball" && gameDecision !== "florida"){
+while(gameName != "powerball" && gameDecision != "florida"){
     gameName = prompt("You did not enter a valid response.\n\nPlease enter Powerball or Florida.");
+    gameName = gameName.toLowerCase();
 }
+
+if(gameName == "powerball"){
+    gameNumbers = gameNumbers(1,35);
+
+}
+
 
